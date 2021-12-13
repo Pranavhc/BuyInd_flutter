87423171536cb66f45e49cd0f096ab4fa370a62a
Pranav Chandane <pranavhchandane@gmail.com>
2021-12-13 16:25:36 +0530

    Firebase can be initialized on both android & web

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: prefer_const_constructors
 import 'package:buyind/Utils/is_logged_in.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -8,17 +9,26 @@ import 'Pages/home_page.dart';
 import 'Pages/login_ui.dart';
 import 'Utils/google_SI_provider.dart';
 import 'Utils/routes.dart';
+import 'package:flutter/foundation.dart' show kIsWeb;
+
+initFireBase() async {
+  // kIsWeb means the platform is website
+  if (kIsWeb) {
+    //to avoid "FirebaseOptions cannot be null" error
+    await Firebase.initializeApp(
+        options: FirebaseOptions(
+      apiKey: "AIzaSyDjX1lo-8PvJLANoY4_AzCk3NhP0F4mpJs",
+      appId: "1:742176317489:web:04486ea88123bfe5b3f510",
+      messagingSenderId: "742176317489",
+      projectId: "buyind-be424",
+    ));
+  } else //android doesn't demand "FirebaseOptions"
+    await Firebase.initializeApp();
+}
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-      // options: FirebaseOptions(
-      //   apiKey: "AIzaSyDjX1lo-8PvJLANoY4_AzCk3NhP0F4mpJs",
-      //   appId: "1:742176317489:web:04486ea88123bfe5b3f510",
-      //   messagingSenderId: "742176317489",
-      //   projectId: "buyind-be424",
-      // ),
-      );
+  await initFireBase();
   runApp(const BuyInd());
 }
 
